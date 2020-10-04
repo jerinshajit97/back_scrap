5f26cfd746f24e3398578c039d29f3e46ec7d63f
jack <jerinshajit97@gmail.com>
2020-10-04 13:02:15 +0530

    first commit

---
+++ backScrap.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+echo "[=] statring Back Scrap"
+
+echo "[+] creating folder"
+mkdir -p wayback
+
+echo "[+] scrapping wayback machine"
+echo "$1" | waybackurls | sort -u > wayback/wayback.txt
+
+
+for list in $(gf --list)
+	do 
+		echo "[+] finding $list"
+
+		if [ $( cat wayback/wayback.txt | gf $list | sort -u | wc -c ) -gt 1 ]
+		then
+			cat wayback/wayback.txt | gf $list | sort -u > wayback/$list.txt
+		fi
+	done
+
+echo "[+] finding paths"
+cat wayback/wayback.txt | unfurl paths | sort -u > wayback/paths.txt
+
+echo "[+] finding keypairs"
+cat wayback/wayback.txt | unfurl keypairs | sort -u > wayback/keypairs.txt
+
+echo "[+] finding domains"
+cat wayback/wayback.txt | unfurl domains | sort -u > wayback/domains.txt
+
+
+echo "[=] back scrap is completed! "
+
+echo "peace be with you"
